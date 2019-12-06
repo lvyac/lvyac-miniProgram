6de0df00b321db8efbe80e50b58f78672da08f7c
lvyac <admin@lvyac.com>
2019-12-06 15:56:50 +0800

    first commit

---
+++ siteinfo.js
@@ -0,0 +1,11 @@
+module.exports = {
+  acid: -1,
+  version: "1.0.0",
+  siteroot: "http://weixin.lvyac.com.com",
+  apiroot: "https://weixin.lvyac.com/web/index.php?store_id=2&r=api/"
+
+  //百宝提示    只需要改域名和storeid即可
+  //独立版：https://域名/web/index.php?store_id=1&r=api/
+  //微擎版：https://域名/addons/zjhj_mall/core/web/index.php?store_id=1&r=api/
+  //更多服务请淘宝店铺搜索百宝网络科技/
+};
